9cce0c2e51ad4cb2b9b48fea4a83c659938ed82f
xogma <xogmadom@naver.com>
2020-09-04 16:22:00 +0900

    탭바 추가, 4개의 뷰 컨트롤러 코코팜으로 생성 및 연결

---
+++ SSAK30/NoticeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NoticeViewController.swift
+//  SSAK30
+//
+//  Created by taeheum on 2020/09/04.
+//  Copyright © 2020 김승희. All rights reserved.
+//
+
+import UIKit
+
+class NoticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
