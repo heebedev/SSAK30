acab54614e5c9ff3ab12463941008659fb137830
김승희 <seungheekim@SH-MacBookPro.local>
2020-09-04 14:30:46 +0900

    first commit

---
+++ SSAK30/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSAK30
+//
+//  Created by 김승희 on 2020/09/04.
+//  Copyright © 2020 김승희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
